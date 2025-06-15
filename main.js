f5e038a7b3216f0bd06a930290f83e0f23823b46
Guillermo Garcia <125451487+guillermo-garciarey@users.noreply.github.com>
2025-06-15 16:01:48 +0100

    Changes

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// Service Worker Setup
+
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		navigator.serviceWorker
+			.register("/service-worker.js")
+			.then((reg) => console.log("Service Worker registered:", reg.scope))
+			.catch((err) =>
+				console.error("Service Worker registration failed:", err)
+			);
+	});
+}
+
 // Toggle Theme (Dark & Light)
 
 const toggleItem = document.getElementById("toggle-theme");
